0b2ce292cc6948b99222fec18d2373ae829b99a4
Jeevan Jyoti Dash <dashjeevanjyoti04@gmail.com>
2020-06-07 11:42:04 +0530

    final commit

--- src/Components/Spinner.js
+++ src/Components/Spinner.js
@@ -2,11 +2,19 @@ import React, { Fragment } from 'react';
 import spinner from '../img/muisc.gif';
 
 export default () => (
-  <Fragment>
+  <div style={{
+    display: 'flex',
+    justifyContent: 'center',
+    height: '100vh'
+    }}>
     <img
       src={spinner}
-      style={{ width: '300px', margin: 'auto', display: 'block' }}
+      style={{ 
+        width: '300px', 
+        margin: 'auto', 
+        display: 'block',
+      }}
       alt='Loading...'
     />
-  </Fragment>
+  </div>
 );
